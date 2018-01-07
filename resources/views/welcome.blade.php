d7de9f903687fc3980ef295923c4843f011342dd
赵子文 <zhaoziwen@zhaoziwendeMacBook-Pro.local>
2018-01-07 20:59:46 +0800

    add some index ui

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,196 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
+<!DOCTYPE html>
+<html lang="zh-cn">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="Author" contect="http://www.webqin.net">
+    <title>三级分销</title>
+    <link rel="shortcut icon" href="images/favicon.ico" />
+    
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/response.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="http://cdn.bootcss.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="http://cdn.bootcss.com/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="maincont">
+     <div class="head-top">
+      <img src="images/head.jpg" />
+      <dl>
+       <dt><a href="user.html"><img src="images/touxiang.jpg" /></a></dt>
+       <dd>
+        <h1 class="username">三级分销终身荣誉会员</h1>
+        <ul>
+         <li><a href="prolist.html"><strong>34</strong><p>全部商品</p></a></li>
+         <li><a href="javascript:;"><span class="glyphicon glyphicon-star-empty"></span><p>收藏本店</p></a></li>
+         <li style="background:none;"><a href="javascript:;"><span class="glyphicon glyphicon-picture"></span><p>二维码</p></a></li>
+         <div class="clearfix"></div>
+        </ul>
+       </dd>
+       <div class="clearfix"></div>
+      </dl>
+     </div><!--head-top/-->
+     <form action="#" method="get" class="search">
+      <input type="text" class="seaText fl" />
+      <input type="submit" value="搜索" class="seaSub fr" />
+     </form><!--search/-->
+     <ul class="reg-login-click">
+      <li><a href="login.html">登录</a></li>
+      <li><a href="reg.html" class="rlbg">注册</a></li>
+      <div class="clearfix"></div>
+     </ul><!--reg-login-click/-->
+     <div id="sliderA" class="slider">
+      <img src="images/image1.jpg" />
+      <img src="images/image2.jpg" />
+      <img src="images/image3.jpg" />
+      <img src="images/image4.jpg" />
+      <img src="images/image5.jpg" />
+     </div><!--sliderA/-->
+     <ul class="pronav">
+      <li><a href="prolist.html">晋恩干红</a></li>
+      <li><a href="prolist.html">万能手链</a></li>
+      <li><a href="prolist.html">高级手镯</a></li>
+      <li><a href="prolist.html">特异戒指</a></li>
+      <div class="clearfix"></div>
+     </ul><!--pronav/-->
+     <div class="index-pro1">
+      <div class="index-pro1-list">
+       <dl>
+        <dt><a href="proinfo.html"><img src="images/pro1.jpg" /></a></dt>
+        <dd class="ip-text"><a href="proinfo.html">这是产品的名称</a><span>已售：488</span></dd>
+        <dd class="ip-price"><strong>¥299</strong> <span>¥599</span></dd>
+       </dl>
+      </div>
+      <div class="index-pro1-list">
+       <dl>
+        <dt><a href="proinfo.html"><img src="images/pro1.jpg" /></a></dt>
+        <dd class="ip-text"><a href="proinfo.html">这是产品的名称</a></dd>
+        <dd class="ip-price"><strong>¥299</strong> <span>¥599</span></dd>
+       </dl>
+      </div>
+      <div class="index-pro1-list">
+       <dl>
+        <dt><a href="proinfo.html"><img src="images/pro1.jpg" /></a></dt>
+        <dd class="ip-text"><a href="proinfo.html">这是产品的名称</a><span>已售：488</span></dd>
+        <dd class="ip-price"><strong>¥299</strong> <span>¥599</span></dd>
+       </dl>
+      </div>
+      <div class="index-pro1-list">
+       <dl>
+        <dt><a href="proinfo.html"><img src="images/pro1.jpg" /></a></dt>
+        <dd class="ip-text"><a href="proinfo.html">这是产品的名称</a></dd>
+        <dd class="ip-price"><strong>¥299</strong> <span>¥599</span></dd>
+       </dl>
+      </div>
+      <div class="index-pro1-list">
+       <dl>
+        <dt><a href="proinfo.html"><img src="images/pro1.jpg" /></a></dt>
+        <dd class="ip-text"><a href="proinfo.html">这是产品的名称</a><span>已售：488</span></dd>
+        <dd class="ip-price"><strong>¥299</strong> <span>¥599</span></dd>
+       </dl>
+      </div>
+      <div class="index-pro1-list">
+       <dl>
+        <dt><a href="proinfo.html"><img src="images/pro1.jpg" /></a></dt>
+        <dd class="ip-text"><a href="proinfo.html">这是产品的名称</a></dd>
+        <dd class="ip-price"><strong>¥299</strong> <span>¥599</span></dd>
+       </dl>
+      </div>
+      <div class="index-pro1-list">
+       <dl>
+        <dt><a href="proinfo.html"><img src="images/pro1.jpg" /></a></dt>
+        <dd class="ip-text"><a href="proinfo.html">这是产品的名称</a><span>已售：488</span></dd>
+        <dd class="ip-price"><strong>¥299</strong> <span>¥599</span></dd>
+       </dl>
+      </div>
+      <div class="index-pro1-list">
+       <dl>
+        <dt><a href="proinfo.html"><img src="images/pro1.jpg" /></a></dt>
+        <dd class="ip-text"><a href="proinfo.html">这是产品的名称</a></dd>
+        <dd class="ip-price"><strong>¥299</strong> <span>¥599</span></dd>
+       </dl>
+      </div>
+      <div class="clearfix"></div>
+     </div><!--index-pro1/-->
+     <div class="prolist">
+      <dl>
+       <dt><a href="proinfo.html"><img src="images/prolist1.jpg" width="100" height="100" /></a></dt>
+       <dd>
+        <h3><a href="proinfo.html">四叶草</a></h3>
+        <div class="prolist-price"><strong>¥299</strong> <span>¥599</span></div>
+        <div class="prolist-yishou"><span>5.0折</span> <em>已售：35</em></div>
+       </dd>
+       <div class="clearfix"></div>
+      </dl>
+      <dl>
+       <dt><a href="proinfo.html"><img src="images/prolist1.jpg" width="100" height="100" /></a></dt>
+       <dd>
+        <h3><a href="proinfo.html">四叶草</a></h3>
+        <div class="prolist-price"><strong>¥299</strong> <span>¥599</span></div>
+        <div class="prolist-yishou"><span>5.0折</span> <em>已售：35</em></div>
+       </dd>
+       <div class="clearfix"></div>
+      </dl>
+      <dl>
+       <dt><a href="proinfo.html"><img src="images/prolist1.jpg" width="100" height="100" /></a></dt>
+       <dd>
+        <h3><a href="proinfo.html">四叶草</a></h3>
+        <div class="prolist-price"><strong>¥299</strong> <span>¥599</span></div>
+        <div class="prolist-yishou"><span>5.0折</span> <em>已售：35</em></div>
+       </dd>
+       <div class="clearfix"></div>
+      </dl>
+     </div><!--prolist/-->
+     <div class="joins"><a href="fenxiao.html"><img src="images/jrwm.jpg" /></a></div>
+     <div class="copyright">Copyright &copy; <span class="blue">这是就是三级分销底部信息</span></div>
+     
+     <div class="height1"></div>
+     <div class="footNav">
+      <dl>
+       <a href="index.html">
+        <dt><span class="glyphicon glyphicon-home"></span></dt>
+        <dd>微店</dd>
+       </a>
+      </dl>
+      <dl>
+       <a href="prolist.html">
+        <dt><span class="glyphicon glyphicon-th"></span></dt>
+        <dd>所有商品</dd>
+       </a>
+      </dl>
+      <dl>
+       <a href="car.html">
+        <dt><span class="glyphicon glyphicon-shopping-cart"></span></dt>
+        <dd>购物车 </dd>
+       </a>
+      </dl>
+      <dl>
+       <a href="user.html">
+        <dt><span class="glyphicon glyphicon-user"></span></dt>
+        <dd>我的</dd>
+       </a>
+      </dl>
+      <div class="clearfix"></div>
+     </div><!--footNav/-->
+    </div><!--maincont-->
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/style.js"></script>
+    <!--焦点轮换-->
+    <script src="js/jquery.excoloSlider.js"></script>
+    <script>
+        $(function () {
+         $("#sliderA").excoloSlider();
+        });
+    </script>
+  </body>
 </html>
